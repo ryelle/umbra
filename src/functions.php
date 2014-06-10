d3b7c643c11638b1f0777924ec020f3f543501fe
ryelle <kelly.dwan@10up.com>
2014-06-09 23:38:15 -0400

    First pass at image-based color scheme

--- src/functions.php
+++ src/functions.php
@@ -212,6 +212,101 @@ function umbra_mce_css( $mce_css ) {
 }
 add_filter( 'mce_css', 'umbra_mce_css' );
 
+function umbra_tonesque_scheme() {
+	if ( ! is_singular() || ! has_post_thumbnail() ) {
+		return;
+	}
+
+	$thumb_attrs = wp_get_attachment_image_src( get_post_thumbnail_id() );
+	$image_src = $thumb_attrs[0];
+
+	require get_template_directory() . '/inc/tonesque/tonesque.php';
+	$image = new Tonesque( $image_src );
+
+	$base_color = $image->color();
+
+	$body_background = new Color( '#' . $base_color, 'hex' );
+	if ( 250 < $body_background->getDistanceRgbFrom( new Color('#ffffff', 'hex') ) ) {
+		$body_background = $body_background->lighten( 25 );
+	} elseif ( 150 < $body_background->getDistanceRgbFrom( new Color('#ffffff', 'hex') ) ) {
+		$body_background = $body_background->lighten( 15 );
+	}
+
+	$title_color = new Color( '#' . $base_color, 'hex' );
+	$site_title_color = new Color( '#' . $base_color, 'hex' );
+	$link_color = new Color( '#' . $base_color, 'hex' );
+	$hover_color = new Color( '#' . $base_color, 'hex' );
+	$description_color = new Color( '#' . $base_color, 'hex' );
+	$nav_text_color = new Color( '#' . $base_color, 'hex' );
+	$nav_bg_color = new Color( '#' . $base_color, 'hex' );
+	$nav_current_bg_color = new Color( '#' . $base_color, 'hex' );
+	$sidebar_bg_color = new Color( '#' . $base_color, 'hex' );
+
+	$sidebar_bg_color = $sidebar_bg_color->darken( 10 )->desaturate( 15 );
+
+	if ( 150 < $sidebar_bg_color->getDistanceRgbFrom( new Color('#000000', 'hex') ) ) {
+		$site_title_color = $site_title_color->lighten( 13 );
+		$description_color = $description_color->desaturate( 25 )->lighten( 15 );
+		$nav_text_color = $nav_text_color->lighten( 25 );
+		$nav_bg_color = $nav_bg_color->darken( 20 )->desaturate( 15 );
+		$nav_current_bg_color = $nav_current_bg_color->darken( 25 )->desaturate( 25 );
+	} else {
+		$site_title_color = $site_title_color->lighten( 35 );
+		$description_color = $description_color->lighten( 30 );
+		$nav_text_color = $nav_text_color->lighten( 60 );
+		$nav_bg_color = $nav_bg_color->lighten( 17 );
+		$nav_current_bg_color = $nav_current_bg_color->lighten( 10 );
+	}
+
+	if ( 150 > $body_background->getDistanceRgbFrom( new Color('#ffffff', 'hex') ) ) {
+		$title_color = $title_color->desaturate( 25 )->darken( 15 );
+		$link_color = $link_color->desaturate( 25 )->darken( 10 );
+		$hover_color = $hover_color->desaturate( 25 )->darken( 20 );
+	} else {
+		$title_color = $title_color->desaturate( 25 )->darken( 5 );
+		$link_color = $link_color->desaturate( 25 )->lighten( 7 );
+		$hover_color = $hover_color->desaturate( 25 );
+	}
+	?>
+<style type="text/css">
+body {
+	background: <?php echo $body_background; ?>;
+}
+a {
+	color: <?php echo $link_color; ?>;
+}
+a:hover, a:active, a:focus {
+	color: <?php echo $hover_color; ?>;
+}
+.entry-title,
+.entry-title a {
+	color: <?php echo $title_color; ?>;
+}
+.site-title a {
+	color: <?php echo $site_title_color; ?>;
+}
+.site-description {
+	color: <?php echo $description_color; ?>;
+}
+.main-navigation ul li a {
+	color: <?php echo $nav_text_color; ?>;
+	background-color: <?php echo $nav_bg_color; ?>;
+}
+.main-navigation ul .current_page_item a,
+.main-navigation ul .current-menu-item a,
+.main-navigation ul li a:hover,
+.main-navigation ul li a:active,
+.main-navigation ul li a:focus {
+	color: <?php echo $nav_text_color; ?>;
+	background-color: <?php echo $nav_current_bg_color; ?>;
+}
+.site-header-bg {
+	background-color: <?php echo $sidebar_bg_color; ?>;
+}
+</style><?php
+}
+add_action( 'wp_head', 'umbra_tonesque_scheme' );
+
 /**
  * Implement the Custom Header feature.
  */
