eb9a2bceb6796287f062ac1f931e2932230f029c
ryelle <kelly.dwan@10up.com>
2014-06-09 20:25:07 -0400

    Call google fonts

--- src/functions.php
+++ src/functions.php
@@ -100,6 +100,118 @@ function umbra_scripts() {
 add_action( 'wp_enqueue_scripts', 'umbra_scripts' );
 
 /**
+ * Returns the Google font stylesheet URL, if available.
+ *
+ * The use of Vollkorn, Fenix and Montserrat by default is
+ * localized. For languages that use characters not supported by either
+ * font, the font can be disabled.
+ *
+ * @since Umbra 1.0
+ *
+ * @return string Font stylesheet or empty string if disabled.
+ */
+function umbra_fonts_url() {
+	$fonts_url = '';
+
+	/* Translators: If there are characters in your language that are not
+	 * supported by Vollkorn, translate this to 'off'. Do not translate
+	 * into your own language.
+	 */
+	$vollkorn = _x( 'on', 'Vollkorn font: on or off', 'umbra' );
+
+	/* Translators: If there are characters in your language that are not
+	 * supported by Fenix, translate this to 'off'. Do not translate into
+	 * your own language.
+	 */
+	$fenix = _x( 'on', 'Fenix font: on or off', 'umbra' );
+
+	/* Translators: If there are characters in your language that are not
+	 * supported by Montserrat, translate this to 'off'. Do not translate
+	 * into your own language.
+	 */
+	$montserrat = _x( 'on', 'Montserrat font: on or off', 'umbra' );
+
+	if ( 'off' !== $vollkorn || 'off' !== $fenix || 'off' !== $montserrat ) {
+		$font_families = array();
+
+		if ( 'off' !== $vollkorn )
+			$font_families[] = 'Vollkorn:700,700italic';
+
+		if ( 'off' !== $fenix )
+			$font_families[] = 'Fenix:400';
+
+		if ( 'off' !== $montserrat )
+			$font_families[] = 'Montserrat:400';
+
+		$protocol = is_ssl() ? 'https' : 'http';
+		$query_args = array(
+			'family' => implode( '|', $font_families ),
+			'subset' => 'latin,latin-ext',
+		);
+		$fonts_url = add_query_arg( $query_args, "$protocol://fonts.googleapis.com/css" );
+	}
+
+	return $fonts_url;
+}
+
+/**
+ * Loads our special font CSS file.
+ *
+ * To disable in a child theme, use wp_dequeue_style()
+ * function mytheme_dequeue_fonts() {
+ *     wp_dequeue_style( 'umbra-fonts' );
+ * }
+ * add_action( 'wp_enqueue_scripts', 'mytheme_dequeue_fonts', 11 );
+ *
+ * @since Umbra 1.0
+ *
+ * @return void
+ */
+function umbra_fonts() {
+	$fonts_url = umbra_fonts_url();
+	if ( ! empty( $fonts_url ) )
+		wp_enqueue_style( 'umbra-fonts', esc_url_raw( $fonts_url ), array(), null );
+}
+add_action( 'wp_enqueue_scripts', 'umbra_fonts' );
+
+/**
+ * Enqueue Google fonts style to admin screen for custom header display.
+ */
+function umbra_admin_fonts( $hook_suffix ) {
+	if ( 'appearance_page_custom-header' != $hook_suffix )
+		return;
+
+	umbra_fonts();
+
+}
+add_action( 'admin_enqueue_scripts', 'umbra_admin_fonts' );
+
+/**
+ * Adds additional stylesheets to the TinyMCE editor if needed.
+ *
+ * @uses umbra_fonts_url() to get the Google Font stylesheet URL.
+ *
+ * @since Umbra 1.0
+ *
+ * @param string $mce_css CSS path to load in TinyMCE.
+ * @return string
+ */
+function umbra_mce_css( $mce_css ) {
+	$fonts_url = umbra_fonts_url();
+
+	if ( empty( $fonts_url ) )
+		return $mce_css;
+
+	if ( ! empty( $mce_css ) )
+		$mce_css .= ',';
+
+	$mce_css .= esc_url_raw( str_replace( ',', '%2C', $fonts_url ) );
+
+	return $mce_css;
+}
+add_filter( 'mce_css', 'umbra_mce_css' );
+
+/**
  * Implement the Custom Header feature.
  */
 //require get_template_directory() . '/inc/custom-header.php';
