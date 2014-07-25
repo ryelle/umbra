4f1aa8c58cf6bf10b58fbf9ea82ea5855e83b954
ryelle <kelly.dwan@gmail.com>
2014-07-25 14:27:26 -0400

    Footer should be “in” the sidebar, same styling as site description

--- src/footer.php
+++ src/footer.php
@@ -8,15 +8,14 @@
  */
 ?>
 
-	</div><!-- #content -->
+		<footer id="colophon" class="site-footer" role="contentinfo">
+			<div class="site-info">
+				<p><a href="<?php echo esc_url( __( 'http://wordpress.org/', 'umbra' ) ); ?>"><?php printf( __( 'Proudly powered by%s', 'umbra' ), '&nbsp;WordPress' ); ?></a></p>
+				<p><?php printf( __( 'Theme: %1$s by %2$s.', 'umbra' ), 'Umbra', '<a href="http://themes.redradar.net" rel="designer">Kelly&nbsp;Dwan & Mel&nbsp;Choyce</a>' ); ?></p>
+			</div><!-- .site-info -->
+		</footer><!-- #colophon -->
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'umbra' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'umbra' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'umbra' ), 'Umbra', '<a href="http://themes.redradar.net" rel="designer">Kelly Dwan & Mel Choyce</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+	</div><!-- #content -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
