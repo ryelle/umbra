5edc28471f0857f9b886386abd1a82aaca3d335f
ryelle <kelly.dwan@gmail.com>
2014-08-01 13:13:42 -0400

    Add an endpoint for generating a stylesheet from a color

--- src/inc/dynamic-colors.php
+++ src/inc/dynamic-colors.php
@@ -10,7 +10,20 @@ class Umbra_ImageColors {
 	private $cache_prefix = 'umbra_css_';
 
 	function __construct() {
+		add_action( 'init',              array( $this, 'rewrites' ) );
+		add_action( 'template_redirect', array( $this, 'ajax_css' ) );
+
 		add_action( 'wp_head', array( $this, 'wp_head' ) );
+	}
+
+	public function rewrites(){
+		if ( ! class_exists( 'Jetpack' ) || ! current_theme_supports( 'tonesque' ) ) {
+			return;
+		}
+
+		add_rewrite_tag( '%umbra%', '([^&]+)' );
+		add_rewrite_tag( '%umbra_color%', '([^&]+)' );
+		add_rewrite_rule( '^umbra-css/([^/]+)/?', 'index.php?umbra=true&umbra_color=$matches[1]', 'top' );
 	}
 
 	public function wp_head() {
@@ -52,6 +65,35 @@ class Umbra_ImageColors {
 		$css = Jetpack_Custom_CSS::minify( $sass, 'sass' );
 
 		return $css;
+	}
+
+	public function ajax_css(){
+		if ( ! get_query_var( 'umbra' ) ){
+			return;
+		}
+
+		$color = get_query_var( 'umbra_color' );
+		$valid_color = preg_match( '/^[0-9a-f]{3}([0-9a-f]{3})?$/i', $color );
+		if ( ! $color || ! $valid_color ) {
+			return;
+		}
+
+		// Cached under `umbra_css_color_123456`
+		$css = get_transient( $this->cache_prefix . 'color_' . $color );
+		if ( empty( $css ) ) {
+			if ( ! class_exists( 'Jetpack_Custom_CSS' ) ) {
+				require Jetpack::get_module_path( 'custom-css' );
+			}
+			$sass = '$base-color: #'. $color .';';
+			$sass .= file_get_contents( get_template_directory() . '/inc/dynamic-colors.scss' );
+			$css = Jetpack_Custom_CSS::minify( $sass, 'sass' );
+
+			set_transient( $this->cache_prefix . 'color_' . $color, $css );
+		}
+
+		//
+		echo $css;
+		die();
 	}
 
 	public function get_base_color( $image_id ){
