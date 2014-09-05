f638ddfea46fd0d1529aa5ce54ad364a421c7b2e
ryelle <kelly.dwan@gmail.com>
2014-09-05 11:51:31 -0400

    Check if Jetpack and Tonesque are available before displaying the customizer settings. If not, show the colors checkbox disabled, with a note to activate Jetpack.

--- src/inc/customizer.php
+++ src/inc/customizer.php
@@ -15,27 +15,55 @@ function umbra_customize_register( $wp_customize ) {
 	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
 	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
 
-	$wp_customize->add_setting( 'umbra_use_tonesque', array(
-		'default' => true,
-	) );
+	$color_label = __( 'Auto color posts', 'umbra' );
+	$color_descrip = __( 'Use the featured image to create the page&rsquo;s color scheme.', 'umbra' );
+	$color_warning = sprintf(
+		'<p style="font-style:normal;font-weight:bold;"><span style="color:#ffba00;" class="dashicons dashicons-info"></span> %s</p>',
+		sprintf( _x( 'Activate %s to enable.', 'Placeholder is a link to Jetpack.me', 'umbra' ), '<a href="http://jetpack.me" target="_blank">Jetpack</a>' )
+	);
 
-	$wp_customize->add_control( 'umbra_use_tonesque', array(
-		'label'      => __( 'Create color scheme from first image', 'umbra' ),
-		'section'    => 'colors',
-		'type'       => 'checkbox',
-	) );
+	if ( ! class_exists( 'Jetpack' ) || ! current_theme_supports( 'tonesque' ) ) {
 
-	$wp_customize->add_setting( 'umbra_base_color', array(
-		'default' => '424046',
-		'sanitize_callback' => 'sanitize_hex_color_no_hash',
-		'sanitize_js_callback' => 'maybe_hash_hex_color',
-		'transport' => 'postMessage',
-	) );
+		$wp_customize->add_setting( 'umbra_use_tonesque', array(
+			'default' => false,
+		) );
 
-	$wp_customize->add_control(new Umbra_Customize_Color_Control( $wp_customize, 'umbra_base_color', array(
-		'label'   => __( 'Default Colors', 'umbra' ),
-		'section' => 'colors',
-	) ) );
+		$wp_customize->add_control( new Umbra_Customize_Control( $wp_customize, 'umbra_use_tonesque', array(
+			'label'       => $color_label,
+			'section'     => 'colors',
+			'type'        => 'checkbox',
+			'description' => $color_descrip . $color_warning,
+			'input_attrs' => array(
+				'disabled' => 'disabled',
+			),
+		) ) );
+
+	} else {
+
+		$wp_customize->add_setting( 'umbra_use_tonesque', array(
+			'default' => true,
+		) );
+
+		$wp_customize->add_control( 'umbra_use_tonesque', array(
+			'label'       => $color_label,
+			'section'     => 'colors',
+			'type'        => 'checkbox',
+			'description' => $color_descrip,
+		) );
+
+		$wp_customize->add_setting( 'umbra_base_color', array(
+			'default' => '424046',
+			'sanitize_callback' => 'sanitize_hex_color_no_hash',
+			'sanitize_js_callback' => 'maybe_hash_hex_color',
+			'transport' => 'postMessage',
+		) );
+
+		$wp_customize->add_control(new Umbra_Customize_Color_Control( $wp_customize, 'umbra_base_color', array(
+			'label'   => __( 'Default Colors', 'umbra' ),
+			'section' => 'colors',
+		) ) );
+
+	}
 }
 add_action( 'customize_register', 'umbra_customize_register' );
 
@@ -84,6 +112,121 @@ if ( class_exists( 'WP_Customize_Color_Control' ) ) {
 			}
 
 			echo str_replace( "color-picker-hex'", "color-picker-hex' data-palettes='$palettes'", $output );
+		}
+	}
+
+	/**
+	 * Create our own Customize Control to use input_attrs on more than just text types.
+	 * We also need to create input_attrs(), for backwards compat with pre-4.0.
+	 */
+	class Umbra_Customize_Control extends WP_Customize_Control {
+		/**
+		 * Render the custom attributes for the control's input element.
+		 * Copied from WP_Customize_Control to provide backcompat
+		 *
+		 * @since 0.1.0
+		 * @access public
+		 */
+		public function input_attrs() {
+			foreach( $this->input_attrs as $attr => $value ) {
+				echo $attr . '="' . esc_attr( $value ) . '" ';
+			}
+		}
+
+		/**
+		 * Render the control's content. Copied from WP_Customize_Control
+		 *
+		 * Allows the content to be overriden without having to rewrite the wrapper in $this->render().
+		 *
+		 * Supports basic input types `text`, `checkbox`, `textarea`, `radio`, and `select`.
+		 * Additional input types such as `email`, `url`, `number`, `hidden` and `date` are supported implicitly.
+		 *
+		 * @since 3.4.0
+		 */
+		protected function render_content() {
+			switch( $this->type ) {
+				case 'checkbox':
+					?>
+					<label>
+						<input type="checkbox" <?php $this->input_attrs(); ?> value="<?php echo esc_attr( $this->value() ); ?>" <?php $this->link(); checked( $this->value() ); ?> />
+						<?php echo esc_html( $this->label ); ?>
+						<?php if ( ! empty( $this->description ) ) : ?>
+							<span class="description customize-control-description"><?php echo $this->description; ?></span>
+						<?php endif; ?>
+					</label>
+					<?php
+					break;
+				case 'radio':
+					if ( empty( $this->choices ) )
+						return;
+
+					$name = '_customize-radio-' . $this->id;
+
+					if ( ! empty( $this->label ) ) : ?>
+						<span class="customize-control-title"><?php echo esc_html( $this->label ); ?></span>
+					<?php endif;
+					if ( ! empty( $this->description ) ) : ?>
+						<span class="description customize-control-description"><?php echo $this->description ; ?></span>
+					<?php endif;
+
+					foreach ( $this->choices as $value => $label ) :
+						?>
+						<label>
+							<input type="radio" <?php $this->input_attrs(); ?> value="<?php echo esc_attr( $value ); ?>" name="<?php echo esc_attr( $name ); ?>" <?php $this->link(); checked( $this->value(), $value ); ?> />
+							<?php echo esc_html( $label ); ?><br/>
+						</label>
+						<?php
+					endforeach;
+					break;
+				case 'select':
+					if ( empty( $this->choices ) )
+						return;
+
+					?>
+					<label>
+						<?php if ( ! empty( $this->label ) ) : ?>
+							<span class="customize-control-title"><?php echo esc_html( $this->label ); ?></span>
+						<?php endif;
+						if ( ! empty( $this->description ) ) : ?>
+							<span class="description customize-control-description"><?php echo $this->description; ?></span>
+						<?php endif; ?>
+
+						<select <?php $this->input_attrs(); ?> <?php $this->link(); ?>>
+							<?php
+							foreach ( $this->choices as $value => $label )
+								echo '<option value="' . esc_attr( $value ) . '"' . selected( $this->value(), $value, false ) . '>' . $label . '</option>';
+							?>
+						</select>
+					</label>
+					<?php
+					break;
+				case 'textarea':
+					?>
+					<label>
+						<?php if ( ! empty( $this->label ) ) : ?>
+							<span class="customize-control-title"><?php echo esc_html( $this->label ); ?></span>
+						<?php endif;
+						if ( ! empty( $this->description ) ) : ?>
+							<span class="description customize-control-description"><?php echo $this->description; ?></span>
+						<?php endif; ?>
+						<textarea rows="5" <?php $this->input_attrs(); ?> <?php $this->link(); ?>><?php echo esc_textarea( $this->value() ); ?></textarea>
+					</label>
+					<?php
+					break;
+				default:
+					?>
+					<label>
+						<?php if ( ! empty( $this->label ) ) : ?>
+							<span class="customize-control-title"><?php echo esc_html( $this->label ); ?></span>
+						<?php endif;
+						if ( ! empty( $this->description ) ) : ?>
+							<span class="description customize-control-description"><?php echo $this->description; ?></span>
+						<?php endif; ?>
+						<input type="<?php echo esc_attr( $this->type ); ?>" <?php $this->input_attrs(); ?> value="<?php echo esc_attr( $this->value() ); ?>" <?php $this->link(); ?> />
+					</label>
+					<?php
+					break;
+			}
 		}
 	}
 }
